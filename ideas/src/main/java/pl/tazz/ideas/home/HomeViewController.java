9c26cb3b4b2b636b62a7d5aa9fa1e88b7e10950f
Maciej Żok <maciuicha@tlen.pl>
2024-12-31 21:38:45 +0100

    Poprawiony wygląd strony głównej

--- ideas/src/main/java/pl/tazz/ideas/home/HomeViewController.java
+++ ideas/src/main/java/pl/tazz/ideas/home/HomeViewController.java
@@ -1,15 +1,30 @@
 package pl.tazz.ideas.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.tazz.ideas.common.controller.IdeasCommonViewController;
+import pl.tazz.ideas.question.service.QuestionService;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/")
-public class HomeViewController {
+public class HomeViewController extends IdeasCommonViewController {
+
+    private final QuestionService questionService;
+
+    public HomeViewController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
 
     @GetMapping("")
-    public String homeView() {
+    public String homeView(Model model) {
+        model.addAttribute("latestQuestions", questionService.getLatestQuestions());
+        addGlobalAttributes(model);
         return "home/index";
     }
 }
